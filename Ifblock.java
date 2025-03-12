1cb056c7934210d96c4bf7350d52ebf2b7aea3cb
shiva kumar <sk1961077@gmail.com>
2025-03-12 21:37:17 +0530

    2nd code

---
+++ Ifblock.java
@@ -0,0 +1,20 @@
+package package1;
+
+public class Ifblock
+{
+ public static void main(String[] args) 
+ {
+	int a = 100;
+	int b = 100;
+	if (a==b);
+	{
+	System.out.println("Hello");
+	}
+	int c = 150;
+	int d = 200;
+	if (c!=d);
+	{
+	System.out.println("not equal");
+	}
+}
+}
